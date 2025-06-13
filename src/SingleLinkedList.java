5e9a9fc4688a108ae64d228c2c0b466a3dccdbbf
Zackery Strickland <Zachary.Strickland@keyin.com>
2025-06-13 21:07:00 -0230

    Added my linked list and set up the functions

---
+++ src/SingleLinkedList.java
@@ -0,0 +1,48 @@
+public class SingleLinkedList {
+    Node head;
+    int size;
+
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+
+    public void delete(int position) {
+        if (position < 0 || position >= size) {
+            System.out.println("Invalid position!");
+            return;
+        }
+
+        if (position == 0) {
+            head = head.next;
+        } else {
+            Node current = head;
+            for (int i = 0; i < position - 1; i++) {
+                current = current.next;
+            }
+            current.next = current.next.next;
+        }
+
+        size--;
+    }
+
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
